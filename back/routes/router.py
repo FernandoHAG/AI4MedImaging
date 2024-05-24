2a4c1b567cbcacd1eee39974e339467d528a95a6
Fernando <fehgomes@gmail.com>
2024-05-24 00:55:01 -0300

    feat: remove unused module
    * update package

--- back/routes/router.py
+++ back/routes/router.py
@@ -1,8 +1,6 @@
-from flask import Blueprint, jsonify
-from users.users import users
+from flask import Blueprint
 
 router = Blueprint('router', __name__, static_folder='static')
-router.register_blueprint(router, url_prefix='/users')
 
 @router.route('/', methods=['GET'])
 def test():
